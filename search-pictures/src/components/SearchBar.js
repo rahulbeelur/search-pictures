9eaf0f33344bcd497f746bea8e284e5c10164662
rahulbeelursuryasoftware <rahul.sb@surya-soft.com>
2022-10-31 15:39:13 +0530

    handled form submit and how to access this.state with arrow function to bind

--- search-pictures/src/components/SearchBar.js
+++ search-pictures/src/components/SearchBar.js
@@ -3,8 +3,9 @@ import React from "react";
 class SearchBar extends React.Component {
     state = { term: "" };
 
-    onFormSubmit(event) {
-        event.preventDefault();
+    onFormSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state.term);
     }
 
     render() {
